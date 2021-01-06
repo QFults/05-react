4e9474f1d334b3db2fb9e2178421f1a531d42bdf
Quinton Fults <quinton.fults.work@gmail.com>
2021-01-06 11:54:32 -0800

    code

--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
+import { useState } from 'react'
+import Navbar from './components/Navbar'
+import Drwr from './components/Drawer'
+
 const App = () => {
+
+  const [drawerState, setDrawerState] = useState({
+    left: false
+  })
+
+  const toggleDrawer = (anchor, open) => (event) => {
+    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+      return;
+    }
+
+    setDrawerState({ ...drawerState, [anchor]: open });
+  }
+
   return (
-    <h1>Hello World!</h1>
+    <>
+      <Navbar
+        toggleDrawer={toggleDrawer} />
+      <Drwr
+        drawerState={drawerState}
+        toggleDrawer={toggleDrawer} />
+    </>
   )
 }
+
+export default App
---
+++ src/components/Drawer/Drawer.js
@@ -0,0 +1,62 @@
+import { useState } from 'react'
+import clsx from 'clsx'
+import { makeStyles } from '@material-ui/core/styles'
+import Drawer from '@material-ui/core/Drawer'
+import Button from '@material-ui/core/Button'
+import List from '@material-ui/core/List'
+import Divider from '@material-ui/core/Divider'
+import ListItem from '@material-ui/core/ListItem'
+import ListItemIcon from '@material-ui/core/ListItemIcon'
+import ListItemText from '@material-ui/core/ListItemText'
+import InboxIcon from '@material-ui/icons/MoveToInbox'
+import MailIcon from '@material-ui/icons/Mail'
+
+const useStyles = makeStyles({
+  list: {
+    width: 250,
+  },
+  fullList: {
+    width: 'auto',
+  },
+})
+
+const Drwr = ({ drawerState, toggleDrawer }) => {
+  const classes = useStyles()
+
+  const list = () => (
+    <div
+      className={classes.list}
+      role="presentation"
+      onClick={toggleDrawer('left', false)}
+      onKeyDown={toggleDrawer('left', false)}
+    >
+      <List>
+        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+          <ListItem button key={text}>
+            <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        {['All mail', 'Trash', 'Spam'].map((text, index) => (
+          <ListItem button key={text}>
+            <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+    </div>
+  )
+
+  return (
+    <>
+      <Drawer anchor={'left'} open={drawerState['left']} onClose={toggleDrawer('left', false)}>
+        {list('left')}
+      </Drawer>
+    </>
+  )
+}
+
+export default Drwr
